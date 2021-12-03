1edbf308dc5421d36fdda152bc2f699b38b13ce5
zulianp <patrick.zulian@gmail.com>
2021-12-03 17:38:20 +0100

    Fixed scripts

--- scripts/run_example_1.sh
+++ scripts/run_example_1.sh
@@ -7,7 +7,7 @@ ny=200
 
 rm data.raw
 
-echo './example_1 '$nx' '$ny
-OMP_PROC_BIND=true mpiexec -np 8 ./example_1 $nx $ny
+echo './sgrid_example_1 '$nx' '$ny
+OMP_PROC_BIND=true mpiexec -np 8 ./sgrid_example_1 $nx $ny
 
 python3  ../scripts/plot.py --path=data.raw --nx=$nx --ny=$ny --output=out.pdf
--- scripts/run_example_2.sh
+++ scripts/run_example_2.sh
@@ -9,9 +9,9 @@ block_size=3
 rm data.raw
 rm data_t*.raw
 
-# mpiexec -np 8 ./example_2 $nx $ny $nz $block_size && \
+# mpiexec -np 8 ./sgrid_example_2 $nx $ny $nz $block_size && \
 #     ls -lah data*.raw
 
-mpiexec -np 16 ./example_2 $nx $ny $nz $block_size && \
+mpiexec -np 16 ./sgrid_example_2 $nx $ny $nz $block_size && \
     python3 ../scripts/transpose_data.py --nx=$nx --ny=$ny --nz=$nz --block_size=$block_size && \
     ls -lah data*.raw
