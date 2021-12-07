180a627a54b1737f16518dac9dd80133efe2f178
zulianp <patrick.zulian@gmail.com>
2021-12-07 11:26:44 +0100

    ...

--- core/sgrid_View.hpp
+++ core/sgrid_View.hpp
@@ -70,6 +70,7 @@ using DeviceExecutionSpace = HostExecutionSpace;
 #else // SGRID_WITH_KOKKOS
 ////////////////////////////////////////////////////////////////////////////
 // Mock protoype. TODO reproduce simplified functionalites of Kokkos
+#error "Not supported yet!"
 
 #define SGRID_INLINE_FUNCTION inline
 #define SGRID_FUNCTION
