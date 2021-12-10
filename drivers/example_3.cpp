3d3d5139769b1bdfbab2f18529549986a50521fc
zulianp <patrick.zulian@gmail.com>
2021-12-10 12:15:18 +0100

    Added new example with long integers

---
+++ drivers/example_3.cpp
@@ -0,0 +1,52 @@
+#include "sgrid_Base.hpp"
+#include "sgrid_Field.hpp"
+#include "sgrid_View.hpp"
+
+#include <fstream>
+#include <mpi.h>
+
+using Real = double;
+
+using Grid_t = sgrid::Grid<Real, 3>;
+using LongIntField_t = sgrid::Field<Grid_t, long>;
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+  sgrid::initialize(argc, argv);
+
+  {
+    const int N_x = 4;
+    const int N_y = 4;
+    const int N_z = 10;
+    const int block_size = 3;
+
+    auto g = std::make_shared<Grid_t>();
+    g->init(MPI_COMM_WORLD, {N_x, N_y, N_z}, {1, 1, 0});
+
+    LongIntField_t x("index", g, block_size, sgrid::BOX_STENCIL);
+    x.allocate_on_device();
+
+    int rank = g->comm_rank();
+
+    // auto horizontal_slice = g->md_range_slice(2);
+
+    auto x_dev = x.view_device();
+    auto g_dev = g->view_device();
+
+    sgrid::parallel_for(
+        "TEST", g->md_range(), SGRID_LAMBDA(int i, int j, int k) {
+          auto b = x_dev.block(i, j, k);
+          // Local index z coordinate
+          b[0] = k;
+          // process rank
+          b[1] = rank;
+          // Global index of z-coordinate
+          b[2] = g_dev.start[2] + k - g_dev.margin[2];
+        });
+
+    x.write("x.raw");
+  }
+
+  sgrid::finalize();
+  return MPI_Finalize();
+}
---
+++ scripts/run_example_3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# run_example_2.sh
+
+nx=4
+ny=4
+nz=10
+block_size=3
+
+rm x.raw
+rm x_t*.raw
+
+mpiexec -np 2 ./sgrid_example_3 $nx $ny $nz $block_size && \
+    python3 ../scripts/transpose_data.py --nx=$nx --ny=$ny --nz=$nz --block_size=$block_size --path=x.raw --output=x_t && \
+    ls -lah x*.raw
