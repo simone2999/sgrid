8d12263c3e14e8e822f8d63684646bc9063cc3cd
Patrick Zulian <patrick.zulian@gmail.com>
2022-05-25 09:29:47 +0200

    added example 13

---
+++ scripts/run_example_13.sh
@@ -0,0 +1,33 @@
+export TMPDIR=/tmp
+
+n_procs=$1
+
+# Args
+# nx=5
+# ny=4
+# nz=$(($n_procs * 3))
+# tile_size=2
+# n_tiles=2
+# block_size=$(($n_procs * $tile_size * $n_tiles))
+
+
+nx=2
+ny=2
+nz=2
+tile_size=10
+n_tiles=5
+block_size=$(($n_procs * $tile_size * $n_tiles))
+
+
+n_elements=$(($nx * $ny * $nz * $block_size))
+echo 'n_entries='$n_elements
+
+# rm ex12*
+
+make -j4 sgrid_example_13 \
+  && mpiexec -np $n_procs ./sgrid_example_13 $nx $ny $nz $tile_size $n_tiles $block_size 
+
+  # \
+  # && python3 ../scripts/transpose_data.py --nx=$nx --ny=$ny --nz=$nz --block_size=$block_size --path=ex12.raw --output=ex12_t \
+  # && ls -lah ex12*.raw 
+ 
