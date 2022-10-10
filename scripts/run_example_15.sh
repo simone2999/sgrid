b6fa5cdede4f7cf4294591834bce044aaf507a22
Dylan Ramelli <rameld@usi.ch>
2022-10-10 08:21:59 +0200

    fixed few things

---
+++ scripts/run_example_15.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# run_example_15.sh
+
+nx=1000
+ny=1000
+nz=1
+block_size=3
+
+rm ../build/example_15/x.raw
+rm ../build/example_15/x_t*.raw
+
+mpiexec -np 6 ./sgrid_example_15 $nx $ny $nz $block_size 
+python3 ../scripts/generate_xdmf.py example_15 x
