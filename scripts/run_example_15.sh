e79fab03f9596d448573f91086102b82460b581d
Dylan Ramelli <rameld@usi.ch>
2022-10-25 09:12:29 +0200

    ...

--- scripts/run_example_15.sh
+++ scripts/run_example_15.sh
@@ -13,4 +13,4 @@ rm ../build/example_15/x_t*.raw
 
 mpiexec -np 6 ./sgrid_example_15 $nx $ny $nz $block_size 
 # Specify folder and filename that was written. Filename preferably without .raw
-python3 ../scripts/generate_xdmf.py example_15 x
+python3 ../scripts/generate_xdmf.py example_15
